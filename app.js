cd1de4fbe9302ab35293008f53ea5c797b4d34c1
ALB Dev <82672349+prodev2004@users.noreply.github.com>
2021-12-12 13:50:25 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,24 @@
+const container = document.querySelector('.container');
+const yes_btn = document.querySelector('.yes_btn');
+const no_btn = document.querySelector('.no_btn');
+
+no_btn.addEventListener('click', () => {
+    no_btn.style.position = 'absolute';
+
+    const random_left = Math.floor(Math.random() * container.clientWidth);
+    const random_top = Math.floor(Math.random() * container.clientHeight);
+
+    if (random_left >= container.clientWidth / 2)
+        no_btn.style.left = `${random_left - no_btn.clientWidth}px`;
+    
+    if (random_top >= container.clientHeight / 2)
+        no_btn.style.top = `${random_top - no_btn.clientHeight}px`;
+})
+
+yes_btn.addEventListener('click', () => {
+    container.innerHTML = `<h2>
+                            I knew it!!! <br>
+                            Lool 😂😂😂 <br>
+                            Please Subscribe...
+                            </h2>`
+})
